fa27e9bbadfb65bb8667e757e6b20500bba1fac4
ReiDy <k022g0060@g.neec.ac.jp>
2023-10-19 14:58:45 +0900

    コメントアウトを追加

--- Header/Object/TitleLogo.cpp
+++ Header/Object/TitleLogo.cpp
@@ -3,10 +3,15 @@
 #include "../../Utils/SoLib/SoLib_Lerp.h"
 
 void TitleLogo::Init() {
+	// 状態管理クラスを生成
+	//state_.release();
 	state_ = std::make_unique<TitleLogoStateManager>();
+	
+	// 最初の状態を登録
 	OpLogoEase* st = new OpLogoEase(this);
 	state_->SetNextState(st);
 
+	// タイトルロゴのスプライトを生成、いろいろ設定
 	titleLogo_.reset(Sprite::Create(TextureManager::Load("uvChecker.png")));
 	titleLogo_->SetPosition({640.0f, 200.0f});
 	titleLogo_->SetScale({ 0.0f, 0.0f });
@@ -16,28 +21,43 @@ void TitleLogo::Init() {
 }
 
 void TitleLogo::Update() {
+	// もしシーンチェンジ指令がきたら
 	if (isChangeSceneCall_ && !preChangeSceneCall_) {
+		// 終了時の状態に切り替える
 		EpLogoState* st = new EpLogoState(this);
 		state_->SetNextState(st);
 	}
 
+	// 状態の更新処理
 	state_->Update();
+
+	// 前フレーム時のシーンチェンジ指令を保存
 	preChangeSceneCall_ = isChangeSceneCall_;
 }
 
 void TitleLogo::Draw() {
+	// タイトルロゴのスプライト描画
 	titleLogo_->Draw();
 }
 
 #pragma region OpLogo
 void OpLogoEase::Init(){
+	// 大きさ
+	titleLogoScale_ = { 0.0f, 0.0f };
 
+	// イージング前の大きさ　
+	start_ = { 0.0f, 0.0f };
+
+	// イージング後の大きさ
+	end_ = { 700.0f,200.f };
 }
 
 void OpLogoEase::Update() {
+	// 大きさのイージング
 	titleLogoScale_.x = start_.x + ((end_.x - start_.x) * SoLib::easeOutBack(t_easing_));
 	titleLogoScale_.y = start_.y + ((end_.y - start_.y) * SoLib::easeOutBack(t_easing_));
 
+	// イージングの媒介変数の処理
 	if (t_easing_ >= 1.0f) {
 		t_easing_ = 1.0f;
 		DefaultUpLogoEase* st = new DefaultUpLogoEase(parent_);
@@ -46,6 +66,8 @@ void OpLogoEase::Update() {
 	else {
 		t_easing_ += 0.1f;
 	}
+
+	// 大きさを設定
 	parent_->SetScale(titleLogoScale_);
 }
 
@@ -55,14 +77,16 @@ void OpLogoEase::Update() {
 void DefaultUpLogoEase::Init() {
 	Vector2 nowPos = parent_->GetPosition();
 	start_ = nowPos;
-	end_ = nowPos - Vector2{0.0f, -5.0f};
+	end_ = nowPos - Vector2{0.0f, -10.0f};
 
 }
 
 void DefaultUpLogoEase::Update() {
+	// 位置のイージング
 	titleLogoPosition_.x = start_.x + ((end_.x - start_.x) * SoLib::easeInExpo(t_easing_));
 	titleLogoPosition_.y = start_.y + ((end_.y - start_.y) * SoLib::easeInExpo(t_easing_));
-
+	
+	// イージングの媒介変数の処理
 	if (t_easing_ >= 1.0f) {
 		t_easing_ = 1.0f;
 		DefaultDownLogoEase* st = new DefaultDownLogoEase(parent_);
@@ -71,8 +95,9 @@ void DefaultUpLogoEase::Update() {
 	else {
 		t_easing_ += 0.01f;
 	}
-	parent_->SetPosition(titleLogoPosition_);
 
+	// 位置を設定
+	parent_->SetPosition(titleLogoPosition_);
 }
 
 void DefaultDownLogoEase::Init() {
@@ -82,9 +107,11 @@ void DefaultDownLogoEase::Init() {
 }
 
 void DefaultDownLogoEase::Update() {
+	// 位置のイージング
 	titleLogoPosition_.x = start_.x + ((end_.x - start_.x) * SoLib::easeInOutBack(t_easing_));
 	titleLogoPosition_.y = start_.y + ((end_.y - start_.y) * SoLib::easeInOutBack(t_easing_));
-
+	
+	// イージングの媒介変数の処理
 	if (t_easing_ >= 1.0f) {
 		t_easing_ = 1.0f;
 		DefaultUpLogoEase* st = new DefaultUpLogoEase(parent_);
@@ -93,8 +120,9 @@ void DefaultDownLogoEase::Update() {
 	else {
 		t_easing_ += 0.01f;
 	}
-	parent_->SetPosition(titleLogoPosition_);
 
+	// 位置を設定
+	parent_->SetPosition(titleLogoPosition_);
 }
 
 #pragma endregion
@@ -109,10 +137,14 @@ void EpLogoState::Init() {
 }
 
 void EpLogoState::Update() {
+	// 位置のイージング
 	titleLogoPosition_.x = start_.x + ((end_.x - start_.x) * SoLib::easeInOutBack(t_easing_));
 	titleLogoPosition_.y = start_.y + ((end_.y - start_.y) * SoLib::easeInOutBack(t_easing_));
-	titleLogoColor_ = SoLib::Lerp(startColor_, endColor_, t_easing_);
-		;
+	
+	// 色のイージング
+	titleLogoColor_ = SoLib::Lerp(startColor_, endColor_, t_colorEasing);
+	
+	// イージングの媒介変数の処理
 	if (t_easing_ >= 1.0f) {
 		t_easing_ = 1.0f;
 		DefaultUpLogoEase* st = new DefaultUpLogoEase(parent_);
@@ -121,28 +153,37 @@ void EpLogoState::Update() {
 	else {
 		t_easing_ += 0.01f;
 	}
+
+	// 色イージングの媒介変数の処理
 	if (t_colorEasing >= 1.0f) {
 		t_colorEasing = 1.0f;
 	}
 	else {
 		t_colorEasing += 0.2f;
 	}
+
+	// 位置、色を設定
 	parent_->SetPosition(titleLogoPosition_);
 	parent_->SetColor(titleLogoColor_);
 }
 #pragma endregion
 
 void TitleLogoStateManager::Update() {
+	// もし次の状態が予約されていたら
 	if (nextState_) {
-		
+		// 予約されている状態を設定
 		state_ = nextState_;
+		// 予約枠はあける
 		nextState_ = nullptr;
 
+		// 状態管理クラスを設定
 		state_->SetStateManager(this);
 
+		// 状態の初期化処理
 		state_->Init();
 	}
 	
+	// 状態の更新処理
 	state_->Update();
 }
 
--- Header/Object/TitleLogo.h
+++ Header/Object/TitleLogo.h
@@ -2,149 +2,217 @@
 #include "../../Engine/DirectBase/2D/Sprite.h"
 #include <memory>
 
+// 前方宣言
 class TitleLogo;
 class TitleLogoStateManager;
 
 // 基底の状態クラス
 class LogoEaseState {
 public:
+	//コンストラクタ
 	LogoEaseState(TitleLogo* const parent) :
 		parent_(parent),
 		isEndMove_(false),
 		t_easing_(0.0f)
 	{}
-
+	
+	// 仮想デストラクタ
 	virtual ~LogoEaseState() {};
 
+	// 状態管理クラスの登録
 	virtual void SetStateManager(TitleLogoStateManager* stateManager) {
 		stateManager_ = stateManager;
 	}
 
+	// 初期化処理
 	virtual void Init() = 0;
+	
+	// 更新処理
 	virtual void Update() = 0;
 
-	virtual bool IsEndMove() { return isEndMove_; }
 
 protected:
 	bool isEndMove_ = false;
 	TitleLogo* const parent_ = nullptr;
+
+	// イージング用の媒介変数
 	float t_easing_ = 0.0f;
 
+	// 状態管理クラス
 	TitleLogoStateManager* stateManager_ = nullptr;
 };
 
 // 最初の状態
 class OpLogoEase : public LogoEaseState {
 public:
+	// 基底クラスのコンストラクタ
 	using LogoEaseState::LogoEaseState;
+
+	// 初期化処理
 	void Init() override;
+
+	// 更新処理
 	void Update() override;
 private:
-
+	// 大きさ
 	Vector2 titleLogoScale_ = { 0.0f, 0.0f };
+	
+	// イージング前の大きさ　
 	Vector2 start_ = { 0.0f, 0.0f };
+
+	// イージング後の大きさ
 	Vector2 end_ = { 700.0f,200.f };
-
-
 };
 
-// 通常の状態
+// 通常のアップ状態
 class DefaultUpLogoEase : public LogoEaseState {
 public:
+	// 基底クラスのコンストラクタ
 	using LogoEaseState::LogoEaseState;
+
+	// 初期化処理
 	void Init() override;
+
+	// 更新処理
 	void Update() override;
 private:
+	// 位置
 	Vector2 titleLogoPosition_ = { 0.0f, 0.0f };
+	
+	// イージング前の位置
 	Vector2 start_ = { 0.0f, 0.0f };
+	
+	// イージング後の位置
 	Vector2 end_ = { 540.f,540.f };
-
-
 };
 
-// 通常の状態
+// 通常のダウン状態
 class DefaultDownLogoEase : public LogoEaseState {
 public:
+	// 基底クラスのコンストラクタ
 	using LogoEaseState::LogoEaseState;
+
+	// 初期化処理
 	void Init() override;
+
+	// 更新処理
 	void Update() override;
 private:
+	// 位置
 	Vector2 titleLogoPosition_ = { 0.0f, 0.0f };
+	
+	// イージング前の位置
 	Vector2 start_ = { 0.0f, 0.0f };
+	
+	// イージング後の位置
 	Vector2 end_ = { 540.f,540.f };
-
-
 };
+
 // 終わる時の状態
 class EpLogoState : public LogoEaseState {
 public:
+	// 基底クラスのコンストラクタ
 	using LogoEaseState::LogoEaseState;
+
+	// 初期化処理
 	void Init() override;
+
+	// 更新処理
 	void Update() override;
 private:
+	// 位置
 	Vector2 titleLogoPosition_ = { 0.0f, 0.0f };
+	
+	// イージング前の位置
 	Vector2 start_ = { 0.0f, 0.0f };
+	
+	// イージング後の位置
 	Vector2 end_ = { 540.f,540.f };
+	
+	// 色(RGBA)
 	Vector4 titleLogoColor_ = { 1.0f, 1.0f, 1.0f, 1.0f };
+	
+	// イージング前の色
 	Vector4 startColor_ = { 1.0f, 1.0f, 1.0f, 1.0f };
+	
+	// イージング後の色
 	Vector4 endColor_ = { 1.0f, 1.0f, 1.0f, 0.0f };
 
+	// 色イージング用の媒介変数
 	float t_colorEasing = 0.0f;
 };
 
-
+// タイトルロゴの状態管理クラス
 class TitleLogoStateManager {
 public:
+	// 次のシーン予約
 	void SetNextState(LogoEaseState* next) {
 		nextState_ = next;
 	}
 
+	// 更新処理
 	void Update();
-
-
-
 private:
+	// 今の状態
 	LogoEaseState* state_;
-	LogoEaseState* nextState_;
+	
+	// 次の状態の予約
+	LogoEaseState* nextState_ = nullptr;
 };
 
+// タイトルロゴ
 class TitleLogo {
-
 public:
+	// 初期化処理
 	void Init();
+
+	// 更新処理
 	void Update();
+
+	// 描画処理
 	void Draw();
 
+	// 位置の取得
 	Vector2 GetPosition() {
 		Vector2 result;
 		result.x = titleLogo_->GetTransform().translate.x;
 		result.y = titleLogo_->GetTransform().translate.y;
 		return result;
 	}
+	// 位置の設定
 	void SetPosition(Vector2 pos) {
 		titleLogo_->SetPosition(pos);
 	}
+	// 大きさの設定
 	void SetScale(Vector2 scale) {
 		titleLogo_->SetScale(scale);
 	}
+	// 角度の設定
 	void SetRotate(float angle) {
 		titleLogo_->SetRotate(angle);
 	}
+	// 色の設定
 	void SetColor(Vector4 color) {
 		titleLogo_->SetColor(color);
 	}
-
-
+	// シーンチェンジ指令
 	void SetIsChangeSceneCall(bool isChange) {
 		isChangeSceneCall_ = isChange;
 	}
 private:
+	// タイトルロゴのスプライト
 	std::unique_ptr<Sprite> titleLogo_ = nullptr;
+	
+	// 位置
 	Vector2 titleLogoPos_ = { 640.0f, 200.0f };
+	
+	// 大きさ
 	Vector2 titleLogoScale_ = { 0.0f, 0.0f };
 	int stateNum_ = -1;
+	// 状態
 	std::unique_ptr<TitleLogoStateManager> state_ = nullptr;
 
+	// シーンチェンジ指令
 	bool isChangeSceneCall_ = false;
 	bool preChangeSceneCall_ = false;
 };
