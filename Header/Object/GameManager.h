75e0127140f60f6783c331f58006f9943200cd28
ReiDy <k022g0060@g.neec.ac.jp>
2023-10-26 09:53:50 +0900

    修正

--- Header/Object/GameManager.h
+++ Header/Object/GameManager.h
@@ -89,7 +89,7 @@ private:
 	bool isClear_ = false;
 	bool isFinish_ = false;
 
-	static uint32_t waveChangeSE_ = 0u;
+	static uint32_t waveChangeSE_;
 
 	const Vector3 kDefaultSpawn_{ 0.f,10.f,0.f };
 
